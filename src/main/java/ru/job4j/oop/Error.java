350e4a29e4aaae762ad46764d95177f5e4542259
Evgenij <kudrinevgenij@gmail.com>
2023-12-17 17:23:26 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "Error 1");
+        Error error3 = new Error(true, 2, "Error 2");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
