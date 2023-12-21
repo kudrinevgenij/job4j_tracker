76d8c821e06c1a2733da6ba0b512ecf7a8e9e000
Evgenij <kudrinevgenij@gmail.com>
2023-12-21 10:13:14 +0700

    2. Модель данных. [#174683]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -1,20 +1,18 @@
 package ru.job4j.pojo;
-
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class College {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setName("Evgenij");
-        student1.setSurname("Kudrin");
-        student1.setPatronymic("Sergeevich");
-        student1.setGroup(1);
-        student1.setDate(new GregorianCalendar(2023, 10, 17));
+        Student student = new Student();
+        student.setName("Evgenij");
+        student.setSurname("Kudrin");
+        student.setPatronymic("Sergeevich");
+        student.setGroup(1);
+        student.setDate(new GregorianCalendar(2023, 10, 17));
 
-        System.out.println("Surname: " + student1.getSurname() + System.lineSeparator() + "Name: " + student1.getName()
-                            + System.lineSeparator() + "Patronymic: " + student1.getPatronymic() + System.lineSeparator()
-                            + "group: " + student1.getGroup() + System.lineSeparator() + "date: "
-                            + (student1.getDate()).getTime());
+        System.out.println("Surname: " + student.getSurname() + System.lineSeparator() + "Name: " + student.getName()
+                            + System.lineSeparator() + "Patronymic: " + student.getPatronymic() + System.lineSeparator()
+                            + "group: " + student.getGroup() + System.lineSeparator() + "date: "
+                            + (student.getDate()).getTime());
     }
 }
