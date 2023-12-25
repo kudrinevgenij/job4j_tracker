24205aedca7d446f9a1b034ebd3fcf348faaccd9
Evgenij <kudrinevgenij@gmail.com>
2023-12-25 12:37:27 +0700

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,13 +1,14 @@
 package ru.job4j.tracker;
-
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
 
     private LocalDateTime created = LocalDateTime.now();
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -39,5 +40,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER)
+                + '}';
     }
 }
