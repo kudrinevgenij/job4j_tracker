57818215c50deff24891a2baec395241f6097793
Evgenij <kudrinevgenij@gmail.com>
2024-01-02 11:31:02 +0700

    Машины

---
+++ src/main/java/ru/job4j/polymorphism/Main.java
@@ -0,0 +1,14 @@
+package ru.job4j.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle sportCar = new SportCar();
+        sportCar.changeGear();
+        sportCar.accelerate();
+        sportCar.steer();
+        sportCar.brake();
+        sportCar.refill();
+        Vehicle.getDragCoefficient();
+        sportCar.chargeBattery();
+    }
+}
