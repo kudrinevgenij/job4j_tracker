0c92dc85bb6d739657ccebe90bfe71b0644f16d6
Evgenij <kudrinevgenij@gmail.com>
2024-01-02 18:48:26 +0700

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.polymorphism;
+
+public class Bus implements Transport {
+    @Override
+    public void drives() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public int refill(int liters) {
+        return 0;
+    }
+}
--- src/main/java/ru/job4j/polymorphism/Transport.java
+++ src/main/java/ru/job4j/polymorphism/Transport.java
@@ -1,5 +1,10 @@
 package ru.job4j.polymorphism;
 
 public interface Transport {
+    void drives();
+
+    void passengers(int number);
+
+    int refill(int liters);
 
 }
