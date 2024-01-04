c60a80a1d5fb018f298eb7a9a5f2d1e68e38fa13
Evgenij <kudrinevgenij@gmail.com>
2024-01-04 21:25:33 +0700

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322 #470174]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -3,6 +3,6 @@ package ru.job4j.oop;
 public class College {
     public static void main(String[] args) {
         Student freshman = new Freshman();
-        Object object = freshman;
+        Object obj = freshman;
     }
 }
--- src/main/java/ru/job4j/oop/HierarchyUsage.java
+++ src/main/java/ru/job4j/oop/HierarchyUsage.java
@@ -15,6 +15,6 @@ public class HierarchyUsage {
 
         /* Ошибка в приведении типа. */
         Object bicycle = new Bicycle();
-        /* Car carBicycle = (Car) bicycle; // код завершится с ошибкой приведения типов ClassCastException */
+        /* Car carBicycle = (Car) bicycle; код завершится с ошибкой приведения типов ClassCastException */
     }
 }
