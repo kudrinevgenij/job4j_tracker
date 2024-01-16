754298601376a57fc98c0ca73c453302b6e58006
Evgenij <kudrinevgenij@gmail.com>
2024-01-16 19:02:36 +0700

    12.2. Тесты вывода на консоль. [#33585]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -19,9 +19,9 @@ public class ReplaceAction implements UserAction {
         String name = input.askStr("Введите имя: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
-            output.println("Заявка изменена успешно");
+            output.println("Заявка изменена успешно.");
         } else {
-            output.println("Ошибка замены заявки");
+            output.println("Ошибка замены заявки.");
         }
         return true;
     }
