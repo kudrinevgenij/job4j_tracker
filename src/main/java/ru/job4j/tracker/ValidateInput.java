4c6004709b7503101287a72a0549d149c4dfeefe
Evgenij <kudrinevgenij@gmail.com>
2024-01-19 22:42:57 +0700

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,16 +1,29 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input input;
+    private final Output output;
+
+    public ValidateInput(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return null;
+    }
+
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = input.askInt(question);
                 invalid = false;
             } catch (NumberFormatException e) {
-                System.out.println("Пожалуйста, введите корректные данные");
+                output.println("Пожалуйста, введите корректные данные");
             }
         } while (invalid);
         return value;
