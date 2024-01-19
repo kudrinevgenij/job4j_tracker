792443dbebb376e4e9276addb89c807ff03c23de
Evgenij <kudrinevgenij@gmail.com>
2024-01-19 14:37:07 +0700

    0.5. Иерархия исключений и множественный catch. [#219368 #474538]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -20,7 +20,7 @@ public class UserStore {
         if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("Invalid user");
         }
-        return user.isValid();
+        return true;
     }
 
     public static void main(String[] args) throws UserNotFoundException {
