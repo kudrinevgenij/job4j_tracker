63b5b581b0262587b5cc418dce787383e42b0601
Evgenij <kudrinevgenij@gmail.com>
2024-01-25 19:05:02 +0700

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -35,19 +35,6 @@ public class PasswordValidator {
             );
         }
 
-        boolean forbiddenSubstring = false;
-        for (String forbidden : FORBIDDEN) {
-            if (password.toLowerCase().contains(forbidden)) {
-                forbiddenSubstring = true;
-            }
-        }
-
-        if (forbiddenSubstring) {
-            throw new IllegalArgumentException(
-                    "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
-            );
-        }
-
         boolean hasUpCase = false;
         boolean hasLowCase = false;
         boolean hasDigit = false;
@@ -85,6 +72,13 @@ public class PasswordValidator {
             throw new IllegalArgumentException(
                     "Password should contain at least one special symbol"
             );
+        }
+        for (String forbidden : FORBIDDEN) {
+            if (password.toLowerCase().contains(forbidden)) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
+                );
+            }
         }
         return password;
     }
