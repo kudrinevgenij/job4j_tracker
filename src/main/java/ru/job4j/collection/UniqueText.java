f25504ad45f09fee77f083be44ddc813e6915bf8
Evgenij <kudrinevgenij@gmail.com>
2024-02-05 21:10:59 +0700

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String string : origin) {
+            check.add(string);
+        }
+        for (String string : text) {
+            result = check.contains(string);
+            if (!result) {
+                break;
+            }
+        }
+        return result;
+    }
+}
