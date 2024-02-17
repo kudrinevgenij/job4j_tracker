dca017dfcc6a297767f979c8cd5b01ae2abac878
Evgenij <kudrinevgenij@gmail.com>
2024-02-17 23:08:45 +0700

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,70 @@
+package ru.job4j.bank;
+
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void deleteUser(String passport) {
+        users.remove(findByPassport(passport));
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite,
+                                 double amount) {
+        Account source = findByRequisite(sourcePassport, sourceRequisite);
+        Account dest = findByRequisite(destinationPassport, destinationRequisite);
+        boolean result = false;
+        if (source != null && dest != null && source.getBalance() >= amount) {
+            source.setBalance(source.getBalance() - amount);
+            dest.setBalance(dest.getBalance() + amount);
+            result = true;
+        }
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
