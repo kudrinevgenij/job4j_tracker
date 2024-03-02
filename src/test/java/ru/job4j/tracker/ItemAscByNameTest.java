823f27b80e79f59f652a4eb87056203d72c879a4
Evgenij <kudrinevgenij@gmail.com>
2024-03-02 22:21:07 +0700

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,29 +12,33 @@ class ItemAscByNameTest {
 
     @Test
     void whenAscCompareByName() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("Third"));
-        items.add(new Item("Second"));
-        items.add(new Item("First"));
-        List<Item> expected = new ArrayList<>();
-        expected.add(items.get(2));
-        expected.add(items.get(1));
-        expected.add(items.get(0));
+        List<Item> items = Arrays.asList(
+                new Item("Third"),
+                new Item("Second"),
+                new Item("First")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("First"),
+                new Item("Second"),
+                new Item("Third")
+        );
         Collections.sort(items, new ItemAscByName());
         assertThat(items).isEqualTo(expected);
     }
 
     @Test
     void whenDescCompareByName() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("First"));
-        items.add(new Item("Second"));
-        items.add(new Item("Third"));
-        List<Item> expected = new ArrayList<>();
-        expected.add(items.get(2));
-        expected.add(items.get(1));
-        expected.add(items.get(0));
+        List<Item> items = Arrays.asList(
+                new Item("First"),
+                new Item("Second"),
+                new Item("Third")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("Third"),
+                new Item("Second"),
+                new Item("First")
+        );
         Collections.sort(items, new ItemDescByName());
-        assertThat(items).isEqualTo(expected);
+        assertThat(items).is(expected);
     }
 }
