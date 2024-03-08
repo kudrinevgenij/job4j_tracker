311ea32fe34b293facd06e835ab830529470de21
Evgenij <kudrinevgenij@gmail.com>
2024-03-08 21:13:22 +0700

    2.0. Лямбда  [#58165]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] attachments = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
+        Arrays.sort(attachments, comparator);
+        Comparator<String> comparatorText = (left, right) -> left.compareTo(right);
+        Comparator<String> comparatorDescSize = (left, right) -> Integer.compare(right.length(), left.length());
+    }
+}
