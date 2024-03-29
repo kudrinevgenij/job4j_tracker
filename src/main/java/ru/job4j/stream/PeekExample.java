e1975ddb8483ff20ab56035acfa1c5bea306be95
Evgenij <kudrinevgenij@gmail.com>
2024-03-29 11:41:32 +0700

    11. Методы forEach() и peek() [#504924]

---
+++ src/main/java/ru/job4j/stream/PeekExample.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PeekExample {
+    public static void main(String[] args) {
+        List<StringBuilder> names = Arrays.asList(
+                new StringBuilder("Михаил"), new StringBuilder("Иван"), new StringBuilder("Елена"));
+        List<String> editedNames = names
+                .stream()
+                .peek((element) -> element.append(" (Ученик Job4j)"))
+                .map(StringBuilder::toString)
+                .sorted()
+                .toList();
+        System.out.println(editedNames);
+    }
+}
